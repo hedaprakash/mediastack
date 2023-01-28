7a6b668f150f9f5f393e81f22b3fc2b71106c029
hedaprakash <prakash@starinnovative.com>
2023-01-28 01:47:47 -0800

    updated path

--- cloud-init/cloud-image-code.ps1
+++ cloud-init/cloud-image-code.ps1
@@ -44,7 +44,7 @@ qm cloudinit dump 9601 user
 
 exit
 
-scp C:/virtualization/cloud-init//userconfig.yaml root@10.10.50.101:/var/lib/vz/snippets/
+scp C:/virtualization/mediastack/cloud-init//userconfig.yaml root@10.10.50.101:/var/lib/vz/snippets/
 ssh root@10.10.50.101 "cat /var/lib/vz/snippets/userconfig.yaml"
 
 ssh root@10.10.50.101
